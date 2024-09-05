0f7d47a17a0c29ba9e528cbc9789f4a49f7bd4a0
TI - VITOR <vitoralgusto472@gmail.com>
2024-09-05 09:39:05 -0300

    Realizado criação de importação de naves da API

--- src/pages/InformacoesNaves.js
+++ src/pages/InformacoesNaves.js
@@ -1,16 +1,51 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-export default function InformacoesNaves({ navigation }){
-  
 
-    
+export default function InformacoesNaves({ route }){
+  const { starships } = route.params;
+  const [ships, setShips] = useState([]);
+
+  useEffect(() => {
+    async function fetchShips() {
+      try {
+        const shipRequests = starships.map(url => axios.get(url));
+        const responses = await Promise.all(shipRequests);
+        const shipsData = responses.map(response => response.data);
+        setShips(shipsData);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    if (starships.length > 0) {
+      fetchShips();
+    }
+  }, [starships]);
+
     return (
-        <View style={styles.container} >
-          <Text>
-              INFORMAÇÃO DAS NAVES
-          </Text>
-        </View>
+      <View style={styles.container}>
+      <Text style={styles.header}>Naves do Personagem</Text>
+      {ships.length > 0 ? (
+        <FlatList
+          data={ships}
+          keyExtractor={item => item.name}
+          renderItem={({ item }) => (
+            <View style={styles.itemContainer}>
+              <Text style={styles.itemName}>{item.name}</Text>
+              <Text>Modelo: {item.model}</Text>
+              <Text>Fabricante: {item.manufacturer}</Text>
+              <Text>Comprimento: {item.length}</Text>
+              <Text>Capacidade de Carga: {item.cargo_capacity}</Text>
+            </View>
+          )}
+        />
+      ) : (
+        <Text>Carregando naves...</Text>
+      )}
+    </View>
+
         );
 }
 const styles = StyleSheet.create({
